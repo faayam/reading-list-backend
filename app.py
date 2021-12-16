edee212523a5057078056f9c9e4fd9aecd28705a
Faysal Mehedi <fmehedi1992@gmail.com>
2021-12-16 19:31:15 +0600

    back to flask_cors

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
-# from flask_cors import CORS 
+from flask_cors import CORS 
 from config import DATABASE_CONNECTION_URI
 
 app = Flask(__name__)
-# CORS(app) 
+CORS(app) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -29,35 +29,23 @@ class Book(db.Model):
         self.rating = rating
 
 
-# @app.route('/')
-# def index():
-#     return jsonify({'msg': "Hello!!!"})
+@app.route('/')
+def index():
+    return jsonify({'msg': "Hello!!!"})
 
-@app.route('/api/add_book', methods=['POST', 'OPTIONS'])
+@app.route('/api/add_book', methods=['POST'])
 def add_book():
 
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
-        
-    elif request.method == "POST": # The actual request following the preflight
-        book_data = request.get_json()
-        new_book = Book(title=book_data['title'], author=book_data['author'], rating=book_data['rating'])
+    book_data = request.get_json()
+    new_book = Book(title=book_data['title'], author=book_data['author'], rating=book_data['rating'])
 
-        db.session.add(new_book)
-        db.session.commit()
+    db.session.add(new_book)
+    db.session.commit()
 
-        response = jsonify({"msg": "Done"})
-        return _corsify_actual_response(response)
+    return jsonify({"msg": "Done"})
 
-    else:
-        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
-
-@app.route('/api/books', methods=['GET', 'OPTIONS'])
+@app.route('/api/books', methods=['GET'])
 def books():
-
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
-    elif request.method == "GET":
 
         book_list = Book.query.all()
         books = []
@@ -65,19 +53,53 @@ def books():
         for book in book_list:
             books.append({'title': book.title, 'author': book.author, 'rating': book.rating})
 
-        response = jsonify({'books' : books})
-        return _corsify_actual_response(response)
+        return jsonify({'books' : books})
 
-def _build_cors_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
+# @app.route('/api/add_book', methods=['POST', 'OPTIONS'])
+# def add_book():
 
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+#     if request.method == "OPTIONS": # CORS preflight
+#         return _build_cors_preflight_response()
+
+#     elif request.method == "POST": # The actual request following the preflight
+#         book_data = request.get_json()
+#         new_book = Book(title=book_data['title'], author=book_data['author'], rating=book_data['rating'])
+
+#         db.session.add(new_book)
+#         db.session.commit()
+
+#         response = jsonify({"msg": "Done"})
+#         return _corsify_actual_response(response)
+
+#     else:
+#         raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
+
+# @app.route('/api/books', methods=['GET', 'OPTIONS'])
+# def books():
+
+#     if request.method == "OPTIONS": # CORS preflight
+#         return _build_cors_preflight_response()
+#     elif request.method == "GET":
+
+#         book_list = Book.query.all()
+#         books = []
+
+#         for book in book_list:
+#             books.append({'title': book.title, 'author': book.author, 'rating': book.rating})
+
+#         response = jsonify({'books' : books})
+#         return _corsify_actual_response(response)
+
+# def _build_cors_preflight_response():
+#     response = make_response()
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     response.headers.add('Access-Control-Allow-Headers', "*")
+#     response.headers.add('Access-Control-Allow-Methods', "*")
+#     return response
+
+# def _corsify_actual_response(response):
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5050')
+    app.run(debug=True, host='0.0.0.0', port='5000')
